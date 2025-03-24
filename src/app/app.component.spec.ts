e86b69faf6a1b4d244b0128c3fc25eca2ae1a6e4
sachindigharra <101275995+sachindigharra@users.noreply.github.com>
2025-03-25 00:05:57 +0530

    User Name Input With BridgeLabz Added

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -17,7 +17,7 @@ describe('AppComponent', () => {
   it(`should have the 'Helloworld' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.message).toEqual('Helloworld');
+    expect(app.title).toEqual('Helloworld');
   });
 
   it('should render title', () => {
